816d27bf51963a3e974c02e37128c3c0d7daedd2
gginisty-sfdc <gginisty@salesforce.com>
2016-08-26 16:46:47 +0200

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -11,12 +11,12 @@ exports.schedule_visit = (sender, values) => {
 };
 
 exports.contact_broker = (sender, values) => {
-    messenger.send({text: "Here is the broker information for this property"}, sender);
+    messenger.send({text: "Voici l'agent en charge de ce bien : "}, sender);
     messenger.send(formatter.formatBroker(), sender);
 };
 
 exports.confirm_visit = (sender, values) => {
-    messenger.send({text: `OK, your appointment is confirmed for ${values[2]}. ${values[1]}.`}, sender);
+    messenger.send({text: `C'est noté, votre rendez-vous est confirmé pour le ${values[2]}. ${values[1]}.`}, sender);
 };
 
 exports.contact_me = (sender, values) => {
@@ -24,7 +24,7 @@ exports.contact_me = (sender, values) => {
     let propertyId = values[1];
     messenger.getUserInfo(sender).then(response => {
         salesforce.createCase(propertyId, response.first_name + " " + response.first_name, sender).then(() => {
-            messenger.send({text: `Thanks for your interest, ${response.first_name}. I asked a broker to contact you asap.`}, sender);
+            messenger.send({text: `Merci pour votre intérêt ${response.first_name}. J'ai demandé à un agent de vous contacter dès que possible`}, sender);
         });
     });
 
