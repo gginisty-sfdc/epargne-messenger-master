f30f2d0b8322a058d2eddf860c8517c3c4372ebf
gginisty-sfdc <gginisty@salesforce.com>
2016-09-20 00:37:01 +0200

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -11,12 +11,12 @@ exports.schedule_visit = (sender, values) => {
 };
 
 exports.contact_broker = (sender, values) => {
-    messenger.send({text: "Voici les compétences requises pour postuler à ce projet : "}, sender);
+    messenger.send({text: "Voici plus de détails sur ce fonds  : "}, sender);
     messenger.send(formatter.formatBroker(), sender);
 };
 
 exports.confirm_visit = (sender, values) => {
-    messenger.send({text: `C'est noté, votre rendez-vous est confirmé pour le ${values[2]}. ${values[1]}.`}, sender);
+    messenger.send({text: `C'est noté, votre rendez-vous est confirmé.`}, sender);
 };
 
 exports.contact_me = (sender, values) => {
@@ -24,7 +24,7 @@ exports.contact_me = (sender, values) => {
     let propertyId = values[1];
     messenger.getUserInfo(sender).then(response => {
         salesforce.createCase(propertyId, response.first_name + " " + response.first_name, sender).then(() => {
-            messenger.send({text: `Merci pour votre intérêt ${response.first_name}. J'ai demandé à un conseiller de vous contacter dès que possible`}, sender);
+            messenger.send({text: `Merci pour votre intérêt ${response.first_name}. J'ai demandé à un expert Natixis de vous contacter dès que possible`}, sender);
         });
     });
 
